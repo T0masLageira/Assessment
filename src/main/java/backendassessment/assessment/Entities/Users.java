286a94b999a8b42377a5747d04e3ed9da01cf85a
T0masLageira <tomas.lageira@solera.com>
2022-11-17 09:54:24 +0100

    Create Users.java

---
+++ src/main/java/backendassessment/assessment/Entities/Users.java
@@ -0,0 +1,75 @@
+package backendassessment.assessment.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Users {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name="Name")
+    private String name;
+
+    @Column(name="surName")
+    private String surName;
+    
+    @Column(name="email")
+    private String phoneNumber;
+
+    @Column(name="email")
+    private String email;
+
+	public Users(Long id, String name, String surName, String phoneNumber, String email) {
+		this.id = id;
+		this.name = name;
+		this.surName = surName;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	} 
+}
